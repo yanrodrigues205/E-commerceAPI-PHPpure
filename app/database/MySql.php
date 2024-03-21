86a7d3d2cc2265b6b1fd130a6bdb39922dcd45cf
yanrodrigues205 <yan.pablo205@gmail.com>
2024-03-21 15:51:36 -0300

    :sparkles: feat: connection and data operations (getAll, getOne, delete)

---
+++ app/database/MySql.php
@@ -0,0 +1,110 @@
+<?php 
+    namespace database;
+
+    use InvalidArgumentException;
+    use PDO;
+    use utils\GlobalConstants;
+
+    class MySql
+    {
+        private object $database;
+
+        public function __construct()
+        {
+            $this->database = $this->setDatabase();
+        }
+
+        /**
+         * @return PDO
+         */
+
+        public function setDatabase() : mixed
+        {
+            try
+            {
+                return new PDO(
+                    'mysql:host='.DATABASE_HOST.'; dbname='.DATABASE_NAME.';', DATABASE_USER, DATABASE_PASS
+                );
+            }
+            catch(\Exception $e)
+            {
+                throw new InvalidArgumentException();
+            }
+        }
+
+        /**
+         * @param string $table
+         * @param string $id
+         * @return string
+         */
+
+        public function Delete(string $table, string $id) : mixed
+        {
+            $query = "DELETE FROM ".$table." WHERE id = :id";
+
+            if(!empty($table) && !empty($id))
+            {
+                $this->database->beginTransaction();
+                $prepare = $this->database->prepare($query);
+                $prepare->bindParam(":id", $id);
+                $prepare->execute();
+
+                if($prepare->rowCount()> 0)
+                {
+                    $this->database->commit();
+                    return "Has been successfully deleted! ID => ".$id."; TABLE => ".$table.";";
+                }
+
+                $this->database->rollBack();
+                throw new InvalidArgumentException("Error, Unable to perform delete operation!");
+            }
+
+            throw new InvalidArgumentException("Error, Fill in the '$table' and '$id' credentials to complete the operation!");
+        }
+
+
+        /**
+         * @param string $table
+         * @return array
+         */
+        public function getAll(string $table) : mixed
+        {
+            $query = "SELECT * FROM " . $table;
+            $prepare = $this->database->query($query);
+
+            $allRecords = $prepare->fetchAll($this->database::FETCH_ASSOC);
+
+            if(is_array($allRecords) && count($allRecords) > 0)
+            {
+                return $allRecords;
+            }
+
+            throw new InvalidArgumentException("Error, Unable to search all records in the table!");
+        }
+
+        /**
+         * @param string $table
+         * @param string $id
+         * @return array
+         */
+
+        public function getOne(string $table, string $id) : mixed
+        {
+            if(!empty($table) && !empty($id))
+            {
+                $query = "SELECT * FROM " . $table . " WHERE id = :id";
+                $prepare = $this->database->prepare($query);
+                $prepare->bindParam(":id", $id);
+                $prepare->execute();
+                $count = $prepare->rowCount();
+
+                if($count === 1)
+                {
+                    return $prepare->fetch($this->database::FETCH_ASSOC);
+                }
+
+                throw new InvalidArgumentException("Error, It was not possible to select the item with id => ".$id.", in the table => ".$table.".");
+            }
+            throw new InvalidArgumentException("Error, Select the table and id to complete selecting a record!");
+        }
+    }
