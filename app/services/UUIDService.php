0654ba51cab64a0c6e71ebc841466c2894ad10dc
yanrodrigues205 <yan.pablo205@gmail.com>
2024-03-24 23:19:12 -0300

    :sparkles: feat: unique identification generation service

---
+++ app/services/UUIDService.php
@@ -0,0 +1,17 @@
+<?php
+    namespace services;
+
+    class UUIDService
+    {
+        public static function create() : string
+        {
+            $data = openssl_random_pseudo_bytes(16);
+            assert(strlen($data) == 16);
+
+            $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+            $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+            return vsprintf("%s%s-%s-%s-%s-%s%s%s", str_split(bin2hex($data), 4));
+        }
+    }
+?>
