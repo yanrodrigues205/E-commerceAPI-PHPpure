d71d52a7bfc61113a0f55ae7ede2d2ef54a60d38
yanrodrigues205 <yan.pablo205@gmail.com>
2024-03-26 12:27:13 -0300

    :sparkles: feat: interpreting route and triggering final destination

--- app/utils/Routes.php
+++ app/utils/Routes.php
@@ -11,7 +11,7 @@
             $urls = self::getUrls();
             $request = [];
             $request['route'] = strtoupper($urls[0]); // first 'directory' in route, SECTOR (users, sales, products)
-            $request['resource'] = $urls[1] ?? null;  // second 'directory' in route or null, OPERATION (create/delete/update/get)
+            $request['resource'] =  strtoupper($urls[1]) ?? null;  // second 'directory' in route or null, OPERATION (create/delete/update/get)
             $request['id'] = $urls[2]; // third 'directory' in route or null, ID OPERATED
             // url: https://localhost/PROJECT_NAME/route/resource/id
             // exemple:  https://localhost/PROJECT_NAME/users/update/ajk591q40-ehwebw39-1jhbqr38
@@ -27,7 +27,7 @@
 
         public static function getUrls()
         {
-            $uri = str_replace("/" . DIR_PROJECT, '', $_SERVER['REQUEST_URI']); //TAKE / GET SELECTED ROUTE
+            $uri = str_replace('/' . DIR_PROJECT, '', $_SERVER['REQUEST_URI']); //TAKE / GET SELECTED ROUTE
 
             return explode("/", trim($uri, "/")); // SPLIT DIRECTORIES ROUTES
         }
--- app/validation/Request.php
+++ app/validation/Request.php
@@ -1,5 +1,6 @@
 <?php
     namespace validation;
+    use controllers\ProductsController;
     use utils\BodyRequest;
     use utils\GlobalConstants;
     use repositories\TokenRepository;
@@ -18,7 +19,6 @@
 
         }
 
-       
         public function processRequest()
         {
             $retorno = utf8_encode(GlobalConstants::MSG_ROUTE_ERROR);
@@ -32,12 +32,65 @@
 
         private function redirectRequest()
         {
-            if($this->request['method'] !== self::GET && $this->request['method'] !== self::DELETE)
+            if($this->request['method'] !== self::GET)
             {
                 $this->body_request = BodyRequest::menageRequestBody();
             }
-            $authorization = getallheaders()['Authorization'] ? getallheaders()['Authorization'] : " ";
+            $authorization = getallheaders()['Authorization'] ? getallheaders()['Authorization'] : " "; // BEARER Authorization
             $this->token_repository->validateToken($authorization);
 
+            $method = $this->request["method"];
+
+            return $this->$method();
+        }
+
+
+        private function get() : void
+        {
+
+            if(in_array($this->request['route'], GlobalConstants::REQUEST_GET))
+            {
+                self::callController();
+            }
+            else
+            {
+                header("HTTP/1.0 404 Not Found");
+                $message = [
+                    "message" => "This route does not exist within the application!",
+                    "status" => 404
+                ];
+                echo json_encode($message);
+                exit;
+            }
+
+        }
+
+        private function post()
+        {
+            echo "passou post!";
+        }
+
+        private function put()
+        {
+            echo "passou put!";
+        }
+
+        private function callController() : void
+        {
+            $controller = ucfirst(strtolower($this->request['route']));
+            $controller_name =  $controller . "Controller";
+            $sla = __DIR__ ."/../controllers/". $controller_name . ".php";
+            $file = $sla;
+            if(file_exists($file))
+            {
+                $controller = "controllers\\".$controller_name;
+                $method = $this->request['resource'];
+                $obj = new $controller();
+                $call_method = $obj->$method();
+            }
+            else
+            {
+                echo "não achou o arquivo!";
+            }
         }
     }
