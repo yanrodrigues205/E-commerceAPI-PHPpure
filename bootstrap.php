5a6f8fa86784e5a7347c5898a0185c07af287b3a
yanrodrigues205 <yan.pablo205@gmail.com>
2024-04-16 09:44:43 -0300

    :books: docs: password and keys from system

--- bootstrap.php
+++ bootstrap.php
@@ -6,10 +6,10 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ERROR);
-define('DATABASE_HOST', 'localhost');
+define('DATABASE_HOST', 'localhost'); //docker = database, local = localhost
 define('DATABASE_NAME', 'fishing-ecommerce');
 define('DATABASE_USER', 'root');
-define('DATABASE_PASS', '');
+define('DATABASE_PASS', ''); //root = '', docker = 2b2232df-eb16-432f-a9f7-1242f3fa54b4
 define('DS', DIRECTORY_SEPARATOR);
 define('DIR_APP', __DIR__);
 define('DIR_PROJECT', 'E-commerceAPI-PHPpure');
