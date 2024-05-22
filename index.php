8b5df0836a066f77f4cc912d256df169ba4deaeb
yanrodrigues205 <yan.pablo205@gmail.com>
2024-05-22 16:18:39 -0300

    :bug: fix: enabling all possible CORS types

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     {
         header('Content-Type: application/json; charset=utf-8');
         header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         $request_validation = new Request(Routes::getRoutes());
         $process = $request_validation->processRequest();
     }
