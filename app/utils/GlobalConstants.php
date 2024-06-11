293d731efcaa2e366d055c51b452ed6e53f648d6
yanrodrigues205 <yan.pablo205@gmail.com>
2024-06-11 13:47:29 -0300

    :bug: fix: removendo partes inutilizadas

--- app/utils/GlobalConstants.php
+++ app/utils/GlobalConstants.php
@@ -6,13 +6,9 @@
          * REQUEST CONSTANTS
          */
         public const REQUEST_TYPE = ['GET', 'POST', 'DELETE', 'PUT'];
-        public const REQUEST_GET = ['USERS', 'PRODUCTS'];
-        public const REQUEST_POST = ['USERS', 'PRODUCTS'];
-        public const REQUEST_PUT = ['USERS', 'PRODUCTS']; //alt
-        public const REQUEST_DELETE = ['USERS', 'PRODUCTS'];
+        public const REQUEST_GET = ['USERS', 'PRODUCTS', 'SALES'];
+        public const REQUEST_POST = ['USERS', 'PRODUCTS', 'SALES'];
+        public const REQUEST_PUT = ['USERS', 'PRODUCTS', 'SALES']; //alt
+        public const REQUEST_DELETE = ['USERS', 'PRODUCTS', 'SALES'];
 
-        /**
-         * MESSAGES CONSTANTS
-         */
-        public const MSG_ROUTE_ERROR = 'The requested route was not found!';
     }
