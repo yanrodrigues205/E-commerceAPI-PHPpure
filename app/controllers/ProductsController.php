818dd878a2b076b09bd904def06deecc3ee6ef85
yanrodrigues205 <yan.pablo205@gmail.com>
2024-06-15 10:28:50 -0300

    :sparkles: feat: updating and removing products

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -1,6 +1,7 @@
 <?php
     namespace controllers;
     use models\ProductsModel;
+    use services\ResponseService;
     class ProductsController extends ProductsModel
     {
         private $dados;
@@ -35,11 +36,10 @@
 
             if(empty($this->dados['name']) || empty($this->dados['description']) || empty($this->dados['value']) || empty($this->dados['amount']) || empty($this->dados['img_path']))
             {
-                header("HTTP/1.1 400 Bad Request");
-                $message = [
-                    "message" => "To complete the precise insertion of the fields (name, description, value, amount, img_path).",
-                    "status" => 400
-                ];
+                ResponseService::send(
+                    "To complete the precise insertion of the fields (name, description, value, amount, img_path).",
+                    400
+                );
             }
             else
             {
@@ -47,26 +47,96 @@
 
                 if($result)
                 {
-                    header('HTTP/1.1 200 OK');
-                    $message = [
-                        "message" => "Product added successfully!",
-                        "status" => 200
-                    ];
+                    ResponseService::send(
+                        "Product added successfully!",
+                        200
+                    );
                 }
                 else
                 {
-                    header('HTTP/1.1 422 Unprocessable Entity');
-                    $message = [
-                        "message" => "Unable to complete product registration!",
-                        "status" => 422
-                    ];
+                    ResponseService::send(
+                        "Unable to complete product registration!",
+                        422
+                    );
                 }
             }
+        }
 
-            echo json_encode($message);
-            exit;
+        public function update(string $request_method) : void
+        {
+            $this->method = "PUT";
+            self::verifyMethod($request_method, $this->method);
+
+            if(empty($this->dados["id"]))
+            {
+                ResponseService::send(
+                    "The product ID is required to complete the operation",
+                    400
+                );
+            }
+
+            if(empty($this->dados["name"]) && empty($this->dados["description"]) && empty($this->dados["value"]) && empty($this->dados["amount"]) && empty($this->dados["img_path"]))
+            {
+                ResponseService::send(
+                    "To make changes you need to change at least one element of a product!",
+                    400
+                );
+            }
+
+            $result = self::updateProduct($this->dados["id"], $this->dados["name"], $this->dados["description"], $this->dados["value"], $this->dados["amount"], $this->dados["img_path"]);
+
+            if(!$result)
+            {
+                ResponseService::send(
+                    "Unable to change product!",
+                    422
+                );
+            }
+
+            ResponseService::send(
+                "Product has been changed successfully!",
+                200
+            );
+        }
+
+        public function delete(string $request_method) : void
+        {
             
-           
+            $this->method = "DELETE";
+            self::verifyMethod($request_method, $this->method);
+            
+            if(empty($this->dados["id"]))
+            {
+                ResponseService::send(
+                    "The product ID is required to complete the operation",
+                    400
+                );
+            }
+
+            $verify_id = self::existsProduct($this->dados["id"]);
+
+            if(!$verify_id)
+            {
+                ResponseService::send(
+                    "There is not product with this ID!",
+                    400
+                );
+            }
+
+            $delete = self::deleteProductByID($this->dados["id"]);
+
+            if(!$delete)
+            {
+                ResponseService::send(
+                    "Unable to demand product deletion!",
+                    422
+                );
+            }
+
+            ResponseService::send(
+                "Product deleted successfully!",
+                200
+            );
         }
 
 
@@ -75,13 +145,10 @@
         {
             if($request_method != $method)
             {
-                header("HTTP/1.0 405 Not Found");
-                    $message = [
-                        "message" => "This route does not exist with the method used!",
-                        "status" => 405
-                    ];
-                echo json_encode($message);
-                exit;
+                ResponseService::send(
+                    "This route does not exist with the method used!",
+                    405
+                );
             }
         }
     }
--- app/models/ProductsModel.php
+++ app/models/ProductsModel.php
@@ -43,6 +43,67 @@
             }
         }
 
+        protected function updateProduct(int $product_id, string | false $name, string | false $description, string | false $value, int | false $amount, string | false $img_path) : bool
+        {
+            $data = [];
+
+            if(!empty($name) && $name !== false)
+            {
+                $data["name"] = $name;
+            }
+
+            if(!empty($description) && $description !== false)
+            {
+                $data["description"] = $description;
+            }
+
+            if(!empty($value) && $value !== false)
+            {
+                $data["value"] = $value;
+            }
+
+            if(!empty($amount) && $amount !== false)
+            {
+                $data["amount"] = $amount;
+            }
+
+            if(!empty($img_path) && $img_path !== false)
+            {
+                $data["img_path"] = $img_path;
+            }
+
+            $query = "UPDATE ".$this->table." SET ";
+
+            $i =0;
+
+            foreach($data as $key_array => $value_array)
+            { 
+                if($i > 0)
+                    $query .= " ,";
+                $query .= $key_array." = :".$key_array;
+                $i++;
+            }
+
+            $query .= " WHERE id = :id";
+            try
+            {
+                $prepare = $this->database ->prepare($query);
+
+                foreach($data as $key_array => $value_array)
+                {
+                    $prepare->bindValue(":".$key_array, $value_array);
+                }
+                $prepare->bindValue(":id", $product_id);
+                $result = $prepare->execute();
+                return $result;
+            }
+            catch(Exception $err)
+            {
+                echo "New Exeption Error in ProductsModel => ".$err;
+                return false;
+            }
+        }
+
         protected function insertProduct(string $name, string $description, string $value, int $amount, string $img_path) : bool
         {
             $query = "INSERT INTO " . $this->table . "(name, description, value, amount, img_path) VALUES (:name, :description, :value, :amount, :img_path) ";
@@ -87,4 +148,27 @@
                 return false;
             }
         }
+
+        protected function deleteProductByID(int $id)
+        {
+            $query = "DELETE FROM `".$this->table."` WHERE id = :id";
+            try
+            {
+                $prepare = $this->database->prepare($query);
+                $prepare->bindParam(":id", $id);
+                $result = $prepare->execute();
+
+                if($result)
+                    return true;
+                else
+                    return false;
+            }
+            catch(Exception $err)
+            {
+                echo "New exception in ProductRepository, Exception => ".$err;
+                return false;
+            }
+        }
+
+
     }
